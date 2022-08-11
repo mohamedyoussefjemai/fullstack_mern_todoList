193ac278ff8a9e7e257daecf0a539a90c6548ce0
youssef <mohamedyoussef.jemai@gmail.com>
2022-08-11 10:25:33 +0200

    auth token

--- routes/auth.js
+++ routes/auth.js
@@ -3,7 +3,8 @@ const router = express.Router()
 const User = require('../models/User')
 const bcrypt = require('bcryptjs')
 const validateRegisterInput = require('../validations/registrationValidation')
-
+const jwt = require('jsonwebtoken')
+const requireAuth = require('../middlewares/permissions')
 // @Route   GET /api/auth/test
 // @desc    Test auth api
 // @access  Public
@@ -31,11 +32,59 @@ router.post('/register', async (req, res) => {
     const hashedPassword = await bcrypt.hash(password, 12)
     const newUser = new User({ email, password: hashedPassword, name })
     const savedUser = await newUser.save()
-    return res.json({ user: savedUser })
+    const returnedUser = { ...savedUser._doc }
+    delete returnedUser.password
+    return res.json({ user: returnedUser })
   } catch (err) {
     console.log(err)
     res.status(500).send(err.message)
   }
+})
+
+// @Route   POST /api/auth/login
+// @desc    login user and return token
+// @access  Public
+router.post('/login', async (req, res) => {
+  try {
+    const { email, password } = req.body
+
+    // find user
+    const user = await User.findOne({ email: new RegExp('^' + email + '$', 'i') })
+    if (!user) {
+      return res.status(400).json({ error: 'a problem with credentials' })
+    }
+
+    const passwordMatch = await bcrypt.compare(password, user.password)
+    if (!passwordMatch) {
+      return res.status(400).json({ error: 'a problem with credentials' })
+    }
+    const payload = { userId: user._id }
+    const token = jwt.sign(payload, process.env.JWT_SECRET, {
+      expiresIn: '7d'
+    })
+    res.cookie('access-token', token, {
+      expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production'
+    })
+
+    const returnedUser = { ...user._doc }
+    delete returnedUser.password
+    return res.json({ token, user: returnedUser })
+  } catch (err) {
+    console.log(err)
+    res.status(500).send(err.message)
+  }
+})
+
+// @Route   GET /api/auth/current
+// @desc    Get current user
+// @access  Private
+router.get('/current', requireAuth, async (req, res) => {
+  if (!req.user) {
+    return res.status(401).send('Unauthorised')
+  }
+  return res.json(req.user)
 })
 
 module.exports = router
